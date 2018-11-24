9455d5bc62ef8191e8a728e3225c14cb04e3ba8a
Mercy Pinargote <mercy_geo@hotmail.com>
2018-11-24 00:07:36 -0500

    Update main.py

--- scr/main.py
+++ scr/main.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Nov 23 22:28:49 2018
 
-@author: Mercy
+@author: Mercy Pinargote
 """
 
 import time
@@ -13,17 +13,20 @@ import pandas as pd
 start_time = time.time()
 util = UtilGetData();
 
+#Archivo que contiene la lista de url para descargar 
 df=pd.read_csv('fileList.csv')
 
+#Se hace un for para descargar los archivos y guardarlos como el mismo tipo o descomprimirlos
 for index, row in df.iterrows():
     urlFileDownload, fileName=row[1],row[2]
     if(row[3]=='zip'):
-        util.getZipFileUnzip(urlFileDownload, fileName)
+        util.getZipFileUnzip(urlFileDownload, fileName)#descarga el archivo, descomprime y guarda en el destino deseado
     else:
-        util.getFile(urlFileDownload, fileName)
+        util.getFile(urlFileDownload, fileName)#descarga el archivo y guarda en el destino deseado
 
 # Show elapsed time
 end_time = time.time()
+#Indica cuanto tiempo se tomo en ejecutar el proceso de descarga
 print ("\nelapsed time: " + \
 			str(round(((end_time - start_time) / 60) , 2)) + " minutes")
 
